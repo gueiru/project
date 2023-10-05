99fe51c0cc142468a6e7e317cb77181a06be5ed2
YiChing0422 <10946037@ntub.edu.tw>
2023-10-05 16:26:18 +0800

    富邦卡更新

--- Python程式碼(采紋宜靜)/卡片+PTT評論/富邦/富邦card(完成).py
+++ Python程式碼(采紋宜靜)/卡片+PTT評論/富邦/富邦card(完成).py
@@ -17,9 +17,11 @@ if response.status_code == 200:
     card_elements = soup.find_all('div', class_='title')
     card_names = [card.text for card in card_elements]
     
-    # 移除不含"卡"字元結尾和以"簽帳金融卡"結尾的項目
-    filtered_card_names = [card for card in card_names if card.endswith("卡") and not card.endswith("簽帳金融卡")]
-    
+    # 移除"簽帳金融卡"、"認同卡"，移除結尾不是"卡"字的怪東東
+    # 邏輯：list中，1.結尾有"卡"「且」2.結尾不是"簽帳金融卡"「或」3."認同卡"
+    filtered_card_names = [card for card in card_names if card.endswith("卡") and 
+                       not (card.endswith("簽帳金融卡") or card.endswith("認同卡") or card.endswith("自在卡"))]
+
     #Use "set" to remove duplicates
     print(list(set(filtered_card_names)))
     print()
