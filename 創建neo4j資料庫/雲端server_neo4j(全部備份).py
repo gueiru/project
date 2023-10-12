19af6e1f827723a3a0c90f573a6d13fb92455bc1
YiChing0422 <10946037@ntub.edu.tw>
2023-10-12 15:48:12 +0800

    neo4j update

--- 創建neo4j資料庫/雲端server_neo4j(全部備份).py
+++ 創建neo4j資料庫/雲端server_neo4j(全部備份).py
@@ -11,23 +11,9 @@ def do_Cypher(tx, text):
     return result
 
 #建立節點函式
-def create_knowledge_point(tx, obj, relation, label):
+def create_knowledge_point(tx, label, relation, obj):
     command = []
-    # 查看 node A 是否存在
-    text = "MATCH (n:"+label+" {name: '"+obj+"'}) RETURN n"
-    result = tx.run(text)
-    # 檢查查詢結果
-    if not result.peek():
-        # 如果節點不存在，則建立它
-        text = "CREATE ("+obj+":"+label+" {name: '"+obj+"'})"
-        tx.run(text)
-        command.append(text)
-        print("Node created.")
-    else:
-        print("Node already exists.")
-    print(command)
-    
-    # 查看 node B 是否存在
+    # 查看 node A 是否存在 (類別)
     text = "MATCH (n:Categorical {name: '"+label+"'}) RETURN n"
     result = tx.run(text)    
     # 檢查查詢結果
@@ -41,8 +27,24 @@ def create_knowledge_point(tx, obj, relation, label):
         print("Node already exists.")
     print(command)
     
+    # 查看 node B 是否存在
+    text = "MATCH (n:"+label+" {name: '"+obj+"'}) RETURN n"
+    result = tx.run(text)
+    # 檢查查詢結果
+    if not result.peek():
+        # 如果節點不存在，則建立它
+        text = "CREATE ("+obj+":"+label+" {name: '"+obj+"'})"
+        tx.run(text)
+        command.append(text)
+        print("Node created.")
+    else:
+        print("Node already exists.")
+    print(command)
+    
+    
+    
     # 查看 node A 與 node B 之間的關係是否存在
-    text = "MATCH (a:"+label+" {name: '"+obj+"'}), (b:Categorical {name: '"+label+"'}) RETURN EXISTS((a)-[:"+relation+"]->(b)) AS relationship_exists"
+    text = "MATCH (a:Categorical {name: '"+label+"'}), (b:"+label+" {name: '"+obj+"'}) RETURN EXISTS((a)-[:"+relation+"]->(b)) AS relationship_exists"
     result = tx.run(text)
     single_result = result.single()
     if single_result is not None:
@@ -52,7 +54,7 @@ def create_knowledge_point(tx, obj, relation, label):
     
     # 如果關聯不存在，則建立它
     if exists is False:
-        text = "MATCH (a:"+label+" {name: '"+obj+"'}), (b:Categorical {name: '"+label+"'}) CREATE (a)-[:"+relation+"]->(b)"
+        text = "MATCH (a:Categorical {name: '"+label+"'}), (b:"+label+" {name: '"+obj+"'}) CREATE (a)-[:"+relation+"]->(b)"
         tx.run(text)
         command.append(text)
         print("Relationship created.")
@@ -68,37 +70,37 @@ def create_knowledge_point(tx, obj, relation, label):
 with driver.session() as session:
     convenience_store = ["SevenEleven", "FamilyMart", "萊爾富", "OK", "美廉社"]
     for cv in convenience_store:
-        session.write_transaction(create_knowledge_point, cv, "is", "超商")
+        session.write_transaction(create_knowledge_point, "超商", "include", cv)
 
 # 國泰卡片
 with driver.session() as session:
     card = ['CUBE卡', '台塑聯名卡', '世界卡', '現金回饋御璽卡', '蝦皮購物聯名卡', 'eTag聯名卡', '雙幣卡', '亞洲萬里通聯名里享卡', '長榮聯名御璽卡', '亞洲萬里通聯名白金卡', '長榮聯名極致御璽卡', '亞洲萬里通聯名鈦商卡']
     for card in card:
-        session.write_transaction(create_knowledge_point, card, "is", "國泰信用卡")
+        session.write_transaction(create_knowledge_point, "國泰信用卡", "include", card)
 
 # 影音串流平台
 with driver.session() as session:
     music = ["Google_Play", "Disney_Plus", "Netflix", "Spotify", "KKBOX", "KKTV"]
     for music in music:
-        session.write_transaction(create_knowledge_point, music, "is", "串流平台")
+        session.write_transaction(create_knowledge_point, "串流平台", "include", music)
 
 #電商平台
 with driver.session() as session:
     shopping_websites = ["蝦皮購物", "momo購物網", "PChome線上購物", "Yahoo奇摩購物中心", "小樹購"]
     for shop in shopping_websites:
-        session.write_transaction(create_knowledge_point, shop, "is", "電商")
+        session.write_transaction(create_knowledge_point, "電商", "include", shop)
 
 #支付方式
 with driver.session() as session:
-    shopping_websites = ["歐付寶", "橘子支付", "ezPay簡單付", "街口支付", "全盈_PAY", "全支付" 
+    PayList = ["歐付寶", "橘子支付", "ezPay簡單付", "街口支付", "全盈_PAY", "全支付" 
                          "PChome國際連", "一卡通Money", "悠遊付", "icash_Pay", "LINE_Pay", 
                          "Apple_Pay", "Samsung_Pay", "Google_Pay", "台灣Pay", "玉山Wallet", 
                          "Hami_Pay"]
-    for shop in shopping_websites:
-        session.write_transaction(create_knowledge_point, shop, "is", "支付方式")
+    for pay in PayList:
+        session.write_transaction(create_knowledge_point, "支付方式", "include", pay)
         
 #交通
 with driver.session() as session:
-    shopping_websites = ["高鐵", "計程車", "公車", "台鐵", "捷運", "飛機"]
-    for shop in shopping_websites:
-        session.write_transaction(create_knowledge_point, shop, "is", "交通")
+    Traffic_List = ["高鐵", "計程車", "公車", "台鐵", "捷運", "飛機"]
+    for transportation in Traffic_List:
+        session.write_transaction(create_knowledge_point, "交通", "include", transportation)
