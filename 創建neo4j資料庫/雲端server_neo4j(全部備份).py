c801e43fa014bffd92d9b5d37a80c26a14172522
YiChing0422 <10946037@ntub.edu.tw>
2023-10-13 13:51:05 +0800

    neo4j update

--- 創建neo4j資料庫/雲端server_neo4j(全部備份).py
+++ 創建neo4j資料庫/雲端server_neo4j(全部備份).py
@@ -34,7 +34,6 @@ def create_knowledge_point(tx, label, relation, obj):
     if not result.peek():
         # 如果節點不存在，則建立它
         text = "CREATE ("+obj+":"+label+" {name: '"+obj+"'})"
-        tx.run(text)
         command.append(text)
         print("Node created.")
     else:
@@ -65,6 +64,45 @@ def create_knowledge_point(tx, label, relation, obj):
 
 
 #-------------------------------以下為建立資料庫的code------------------------------------------
+# 國泰卡片
+with driver.session() as session:
+    card8 = ["蝦皮購物聯名卡", "長榮航空聯名卡", "CUBE卡", "國泰世界卡",
+            "亞洲萬里通聯名卡", "台塑聯名卡", "雙幣卡", "eTag聯名卡"]
+    for card in card8:
+        session.write_transaction(create_knowledge_point, "國泰信用卡", "include", card)
+
+# 富邦卡片
+with driver.session() as session:
+    card20 = [ "富邦J卡", "富邦IMPERIAL尊御世界卡", "富邦數位生活卡",
+            "富邦鑽保卡", "富邦財神系列卡", "Open_Possible聯名卡",
+            "富邦世界卡", "富邦無限卡", "富邦富利生活系列卡", "富邦鈦金卡",
+            "富邦銀行卡", "富邦Costco聯名卡", "momo卡", "富邦悍將悠遊聯名卡",
+            "台茂聯名卡", "廣三SOGO聯名卡", "采盟聯名卡",
+            "DHC聯名卡", "福華聯名卡", "麗嬰房聯名卡"]
+    for card in card20:
+        session.write_transaction(create_knowledge_point, "富邦信用卡", "include", card)
+
+# 中信卡片
+with driver.session() as session:
+    card49 = ["Agoda聯名卡", "Mitsui_Shopping_Park_LaLaport聯名卡", "ALL_ME卡",
+             "LINE_Pay信用卡", "英雄聯盟信用卡", "中油聯名卡",
+             "中信商旅鈦金卡", "TAIPEI_101聯名卡(尊榮鼎極卡)", "TAIPEI_101聯名卡(新鼎極卡)",
+            "TAIPEI_101聯名卡(新御璽卡)", "和泰聯名卡","中國信託鼎極卡(無限卡/極緻卡)",
+            "中國信託鼎極卡(世界卡)", "中信紅利御璽卡","中信紅利晶緻卡",
+            "中信現金回饋御璽卡","中信現金回饋鈦金卡","中信紅利卡",
+            "寰遊美國運通卡","Super_Life_VISA卡", "中信兄弟聯名卡(鼎極卡)",
+            "中信兄弟聯名卡(御璽/鈦金卡)","中信兄弟聯名卡(白金卡)","中信兄弟聯名卡(不限卡別)",
+            "LEXUS聯名卡","中信商務卡/雙幣商務卡","中華電信聯名卡",
+            "漢神百貨聯名卡(無限卡)","漢神百貨聯名卡(世界卡)", "漢神百貨聯名卡(御璽卡)",
+            "漢神百貨聯名卡(晶緻卡)", "漢神百貨聯名卡(鈦金卡)","Global_Mall聯名卡(無限卡)",
+            "Global_Mall聯名卡(御璽卡)","Global_Mall聯名卡(白金卡)","秀泰聯名卡(晶緻卡)",
+            "秀泰聯名卡(白金卡)","大葉髙島屋百貨聯名卡(世界卡)", "大葉髙島屋百貨聯名卡(無限卡)",
+            "大葉髙島屋百貨聯名卡(鈦金卡)","大葉髙島屋百貨聯名卡(御璽卡/晶緻卡)","大葉髙島屋百貨聯名卡(白金卡)",
+            "南紡購物中心聯名卡(鼎極無限卡)","南紡購物中心聯名卡(御璽卡)","勤美天地聯名卡(御璽卡/晶緻卡)",   
+            "勤美天地聯名卡(白金卡)","MUJI無印良品聯名卡", "酷玩卡",
+            "統一企業認同卡"]
+    for card in card49:
+        session.write_transaction(create_knowledge_point, "中信信用卡", "include", card)
 
 #超商
 with driver.session() as session:
@@ -72,16 +110,10 @@ with driver.session() as session:
     for cv in convenience_store:
         session.write_transaction(create_knowledge_point, "超商", "include", cv)
 
-# 國泰卡片
-with driver.session() as session:
-    card = ['CUBE卡', '台塑聯名卡', '世界卡', '現金回饋御璽卡', '蝦皮購物聯名卡', 'eTag聯名卡', '雙幣卡', '亞洲萬里通聯名里享卡', '長榮聯名御璽卡', '亞洲萬里通聯名白金卡', '長榮聯名極致御璽卡', '亞洲萬里通聯名鈦商卡']
-    for card in card:
-        session.write_transaction(create_knowledge_point, "國泰信用卡", "include", card)
-
 # 影音串流平台
 with driver.session() as session:
-    music = ["Google_Play", "Disney_Plus", "Netflix", "Spotify", "KKBOX", "KKTV"]
-    for music in music:
+    musics = ["Google_Play", "Disney_Plus", "Netflix", "Spotify", "KKBOX", "KKTV"]
+    for music in musics:
         session.write_transaction(create_knowledge_point, "串流平台", "include", music)
 
 #電商平台
@@ -104,3 +136,7 @@ with driver.session() as session:
     Traffic_List = ["高鐵", "計程車", "公車", "台鐵", "捷運", "飛機"]
     for transportation in Traffic_List:
         session.write_transaction(create_knowledge_point, "交通", "include", transportation)
+
+
+
+print("done")
