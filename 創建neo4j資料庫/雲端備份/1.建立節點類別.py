e616cd5dafb1309337f7f0bac8ff7059329173ea
YiChing0422 <10946037@ntub.edu.tw>
2023-10-24 14:36:20 +0800

    neo4j update

--- 創建neo4j資料庫/雲端備份/1.建立節點類別.py
+++ 創建neo4j資料庫/雲端備份/1.建立節點類別.py
@@ -13,14 +13,15 @@ def do_Cypher(tx, text):
 #建立節點函式
 def create_knowledge_point(tx, label, relation, obj):
     command = []
+    
     # 查看 node A 是否存在 (類別)
-    text = "MATCH (n:Categorical {name: '"+label+"'}) RETURN n"
-    result = tx.run(text)    
+    text = "MATCH (n:Categorical {name: $label}) RETURN n"
+    result = tx.run(text, label=label)    
     # 檢查查詢結果
     if not result.peek():
         # 如果節點不存在，則建立它
-        text = "CREATE ("+label+":Categorical {name: '"+label+"'})"
-        tx.run(text)
+        text = "CREATE (" + label + ":Categorical {name: $label})"
+        tx.run(text, label=label)
         command.append(text)
         print("Node created.")
     else:
@@ -28,23 +29,23 @@ def create_knowledge_point(tx, label, relation, obj):
     print(command)
     
     # 查看 node B 是否存在
-    text = "MATCH (n:"+label+" {name: '"+obj+"'}) RETURN n"
-    result = tx.run(text)
+    text = "MATCH (n:"+label+" {name: $obj}) RETURN n"
+    result = tx.run(text, obj=obj)
     # 檢查查詢結果
     if not result.peek():
         # 如果節點不存在，則建立它
-        text = "CREATE ("+obj+":"+label+" {name: '"+obj+"'})"
+        text = "CREATE (" + obj + ":" + label + " {name: $obj})"
+        tx.run(text, obj=obj)
         command.append(text)
         print("Node created.")
     else:
         print("Node already exists.")
     print(command)
-    
-    
-    
+
+
     # 查看 node A 與 node B 之間的關係是否存在
-    text = "MATCH (a:Categorical {name: '"+label+"'}), (b:"+label+" {name: '"+obj+"'}) RETURN EXISTS((a)-[:"+relation+"]->(b)) AS relationship_exists"
-    result = tx.run(text)
+    text = "MATCH (a:Categorical {name: $label}), (b:" + label + " {name: $obj}) RETURN EXISTS((a)-[:" + relation + "]->(b)) AS relationship_exists"
+    result = tx.run(text, label=label, obj=obj)
     single_result = result.single()
     if single_result is not None:
         exists = single_result["relationship_exists"]
@@ -53,15 +54,14 @@ def create_knowledge_point(tx, label, relation, obj):
     
     # 如果關聯不存在，則建立它
     if exists is False:
-        text = "MATCH (a:Categorical {name: '"+label+"'}), (b:"+label+" {name: '"+obj+"'}) CREATE (a)-[:"+relation+"]->(b)"
-        tx.run(text)
+        text = "MATCH (a:Categorical {name: $label}), (b:" + label + " {name: $obj}) CREATE (a)-[:" + relation + "]->(b)"
+        tx.run(text, label=label, obj=obj)
         command.append(text)
         print("Relationship created.")
     else:
         print("Relationship already exists.")
     
     print(command)
-
 
 #-------------------------------以下為建立資料庫的code------------------------------------------
 # 國泰卡片
@@ -79,34 +79,34 @@ with driver.session() as session:
 
 # 富邦卡片
 with driver.session() as session:
-    card20 = [ "富邦J卡", "富邦IMPERIAL尊御世界卡", "富邦數位生活卡",
-            "富邦鑽保卡", "富邦財神系列卡", "Open_Possible聯名卡",
-            "富邦世界卡", "富邦無限卡", "富邦富利生活系列卡", "富邦鈦金卡",
-            "富邦銀行卡", "富邦Costco聯名卡", "momo卡", "富邦悍將悠遊聯名卡",
-            "台茂聯名卡", "廣三SOGO聯名卡", "采盟聯名卡",
-            "DHC聯名卡", "福華聯名卡", "麗嬰房聯名卡"]
+    card20 = [  "富邦J卡", "富邦IMPERIAL尊御世界卡", "富邦數位生活卡",
+                "富邦鑽保卡", "富邦財神系列卡", "Open_Possible聯名卡",
+                "富邦世界卡", "富邦無限卡", "富邦富利生活系列卡", "富邦鈦金卡",
+                "富邦銀行卡", "富邦Costco聯名卡", "momo卡", "富邦悍將悠遊聯名卡",
+                "台茂聯名卡", "廣三SOGO聯名卡", "采盟聯名卡",
+                "DHC聯名卡", "福華聯名卡", "麗嬰房聯名卡"]
     for card in card20:
         session.write_transaction(create_knowledge_point, "富邦信用卡", "include", card)
 
 # 中信卡片
 with driver.session() as session:
-    card49 = ["Agoda聯名卡", "Mitsui_Shopping_Park_LaLaport聯名卡", "ALL_ME卡",
-             "LINE_Pay信用卡", "英雄聯盟信用卡", "中油聯名卡",
-             "中信商旅鈦金卡", "TAIPEI_101聯名卡(尊榮鼎極卡)", "TAIPEI_101聯名卡(新鼎極卡)",
-            "TAIPEI_101聯名卡(新御璽卡)", "和泰聯名卡","中國信託鼎極卡(無限卡/極緻卡)",
-            "中國信託鼎極卡(世界卡)", "中信紅利御璽卡","中信紅利晶緻卡",
-            "中信現金回饋御璽卡","中信現金回饋鈦金卡","中信紅利卡",
-            "寰遊美國運通卡","Super_Life_VISA卡", "中信兄弟聯名卡(鼎極卡)",
-            "中信兄弟聯名卡(御璽/鈦金卡)","中信兄弟聯名卡(白金卡)","中信兄弟聯名卡(不限卡別)",
-            "LEXUS聯名卡","中信商務卡/雙幣商務卡","中華電信聯名卡",
-            "漢神百貨聯名卡(無限卡)","漢神百貨聯名卡(世界卡)", "漢神百貨聯名卡(御璽卡)",
-            "漢神百貨聯名卡(晶緻卡)", "漢神百貨聯名卡(鈦金卡)","Global_Mall聯名卡(無限卡)",
-            "Global_Mall聯名卡(御璽卡)","Global_Mall聯名卡(白金卡)","秀泰聯名卡(晶緻卡)",
-            "秀泰聯名卡(白金卡)","大葉髙島屋百貨聯名卡(世界卡)", "大葉髙島屋百貨聯名卡(無限卡)",
-            "大葉髙島屋百貨聯名卡(鈦金卡)","大葉髙島屋百貨聯名卡(御璽卡/晶緻卡)","大葉髙島屋百貨聯名卡(白金卡)",
-            "南紡購物中心聯名卡(鼎極無限卡)","南紡購物中心聯名卡(御璽卡)","勤美天地聯名卡(御璽卡/晶緻卡)",   
-            "勤美天地聯名卡(白金卡)","MUJI無印良品聯名卡", "酷玩卡",
-            "統一企業認同卡"]
+    card49 = [  "Agoda聯名卡", "Mitsui_Shopping_Park_LaLaport聯名卡", "ALL_ME卡",
+                "LINE_Pay信用卡", "英雄聯盟信用卡", "中油聯名卡",
+                "中信商旅鈦金卡", "TAIPEI_101聯名卡_尊榮鼎極卡", "TAIPEI_101聯名卡_新鼎極卡",
+                "TAIPEI_101聯名卡_新御璽卡", "和泰聯名卡","中國信託鼎極卡_無限卡_極緻卡",
+                "中國信託鼎極卡_世界卡", "中信紅利御璽卡","中信紅利晶緻卡",
+                "中信現金回饋御璽卡","中信現金回饋鈦金卡","中信紅利卡",
+                "寰遊美國運通卡","Super_Life_VISA卡", "中信兄弟聯名卡_鼎極卡",
+                "中信兄弟聯名卡_御璽_鈦金卡","中信兄弟聯名卡_白金卡","中信兄弟聯名卡_不限卡別",
+                "LEXUS聯名卡","中信商務卡_雙幣商務卡","中華電信聯名卡",
+                "漢神百貨聯名卡_無限卡","漢神百貨聯名卡_世界卡", "漢神百貨聯名卡_御璽卡",
+                "漢神百貨聯名卡_晶緻卡", "漢神百貨聯名卡_鈦金卡","Global_Mall聯名卡_無限卡",
+                "Global_Mall聯名卡_御璽卡","Global_Mall聯名卡_白金卡","秀泰聯名卡_晶緻卡",
+                "秀泰聯名卡_白金卡","大葉髙島屋百貨聯名卡_世界卡", "大葉髙島屋百貨聯名卡_無限卡",
+                "大葉髙島屋百貨聯名卡_鈦金卡","大葉髙島屋百貨聯名卡_御璽卡_晶緻卡","大葉髙島屋百貨聯名卡_白金卡",
+                "南紡購物中心聯名卡_鼎極無限卡","南紡購物中心聯名卡_御璽卡","勤美天地聯名卡_御璽卡_晶緻卡",   
+                "勤美天地聯名卡_白金卡","MUJI無印良品聯名卡", "酷玩卡",
+                "統一企業認同卡"]
     for card in card49:
         session.write_transaction(create_knowledge_point, "中信信用卡", "include", card)
 
@@ -164,4 +164,4 @@ with driver.session() as session:
         session.write_transaction(create_knowledge_point, "百貨公司", "include", shop)
 
 
-print("done")
+print("------------------done------------------")
