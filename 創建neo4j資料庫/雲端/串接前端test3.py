763f2bbf8485a344fbf715ff2a1363d5a198bc98
YiChing0422 <10946037@ntub.edu.tw>
2023-11-15 17:10:23 +0800

    neo4j 大致完成

--- 創建neo4j資料庫/雲端/串接前端test3.py
+++ 創建neo4j資料庫/雲端/串接前端test3.py
@@ -6,19 +6,23 @@ import matplotlib.pyplot as plt
 uri = "neo4j+s://cd122923.databases.neo4j.io"
 driver = GraphDatabase.driver(uri, auth=("neo4j", "XMvLaxouvDASwAcmMpcndl7W9j6pf6RpLs7ahPjjxQg"))
 
-# 定義一個查詢函式
 def run_query(tx, search_keyword):
     query = (
         "MATCH (source)-[:reward]->(destination) "
         "WHERE toLower(destination.name) CONTAINS toLower($search_keyword) "
-        "RETURN source, destination"
+        "WITH source, destination, labels(destination) as destLabels "
+        "RETURN source, destination, "
+        "CASE WHEN 'Categorical' IN destLabels "
+        "     THEN NULL "
+        "     ELSE [(destination)-[:include]->(categoricalNode:Categorical)-[:reward]->(categoricalCard:card) | categoricalCard] "
+        "END as categoricalCards"
     )
 
     result = tx.run(query, search_keyword=search_keyword)
     return result.data()
 
 # 輸入關鍵詞
-search_keyword = "加油站"
+search_keyword = "住宿"
 with driver.session() as session:
     result = session.read_transaction(run_query, search_keyword)
 
